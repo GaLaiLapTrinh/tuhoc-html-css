49724db01a8ee4b76eeff25c8bb38adbe637008f
GaLaiLapTrinh <galailaptrinh@gmail.com>
2025-04-27 08:52:39 +0700

    update tailwind up to bai128-positioning

---
+++ 0.Full code ly thuyet trong seri bai giang web/bai127-tailwind-config/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        tuychon: "#2233CC",
+        tuychon2: {
+          100: "#22FFCC",
+          500: "#22FFFF",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
